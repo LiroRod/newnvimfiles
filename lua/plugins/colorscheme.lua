65a1356be70a05ac1d6e4e0c3e821b5927528ac7
Liro Rodrigues <liro_rod.dev@hotmail.com>
2025-10-02 20:43:56 -0300

    yay

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+    -- {
+    --     "nyoom-engineering/oxocarbon.nvim",
+    -- },
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         colorscheme = "oxocarbon",
+    --     },
+    -- },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+            ...,
+        },
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "gruvbox",
+            },
+        },
+    },
+}
